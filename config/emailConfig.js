b1593e4d6ac13273f9c51b943d10ac2ec6730b10
Mafuzur Rahman <mafuzurrahman0990@gmail.com>
2024-04-04 21:42:46 +0600

    changed something

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASS
